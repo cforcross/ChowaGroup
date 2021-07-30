dadc0ae206cabc95faccd6f0ff0ff9a32617a69f
cforcross <ntsendifor@gmail.com>
2021-07-30 15:47:26 +0300

    main yml file updated

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .forms import RegisterForm
 
-# Create your tests here.
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(email='normal@user.com', password='foo',username='foo',last_name='bar',first_name='bear')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertEqual(user.last_name, 'bar')
+        self.assertEqual(user.first_name, 'bear')
+        self.assertFalse(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superadmin)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(email='supr@user.com', password='foo',username='el',last_name='bar',first_name='bear')
+        self.assertEqual(admin_user.email, 'supr@user.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superadmin)
+        
+class UserRegistrationTest(TestCase):
+
+    def test_register_user(self):
+        form = RegisterForm(data={
+            "first_name": "chowa",
+            "last_name": "cross",
+            "phone_number": "78562216365",
+            "email": "cross@yahoo.com",
+            "password": "manofsteel5",
+            "confirm_password": "manofsteel5",
+            })
--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -20,13 +21,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '56u^)rsf_s24rl4a4&ejzqrx=a&e)f+4*4^0n3dzd26(44v@4e'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = []
 
+# https://simpleisbetterthancomplex.com/2015/11/26/package-of-the-week-python-decouple.html ⭐⭐⭐
 
 # Application definition
 
@@ -88,17 +88,36 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecom',
+#         'USER': 'django_dev',
+#         'PASSWORD': '7204$BGM$AGUK',
+#         'HOST': 'localhost',
+#         'PORT':'5432'
+#     }
+# }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': config('DB_NAME'),
+#         'USER': config('DB_USER'),
+#         "PASSWORD": config('DB_PASSWORD'),
+#         "HOST": config('DB_HOST'),
+#         "PORT": config('DB_PORT')
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecom',
-        'USER': 'django_dev',
-        'PASSWORD': '7204$BGM$AGUK',
-        'HOST': 'localhost',
-        'PORT':'5432'
+        'ENGINE': config("DB_ENGINE"),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
