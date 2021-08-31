d62cd6fb51f5f098521344b7f7109d1353de2d28
cforcross <ntsendifor@gmail.com>
2021-08-31 12:16:30 +0300

    changes

--- accounts/views.py
+++ accounts/views.py
@@ -64,17 +64,66 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
+
         user = auth.authenticate(email=email, password=password)
 
         if user is not None:
-            auth.login(request,user)
-            messages.success(request,f'Successfully logged in as {user} ')
-            return redirect('dashboard')
+            try:
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+
+                    # Getting the product variations by cart id
+                    product_variation = []
+                    for item in cart_item:
+                        variation = item.variations.all()
+                        product_variation.append(list(variation))
+
+                    # Get the cart items from the user to access his product variations
+                    cart_item = CartItem.objects.filter(user=user)
+                    ex_var_list = []
+                    id = []
+                    for item in cart_item:
+                        existing_variation = item.variations.all()
+                        ex_var_list.append(list(existing_variation))
+                        id.append(item.id)
+
+                    # product_variation = [1, 2, 3, 4, 6]
+                    # ex_var_list = [4, 6, 3, 5]
+
+                    for pr in product_variation:
+                        if pr in ex_var_list:
+                            index = ex_var_list.index(pr)
+                            item_id = id[index]
+                            item = CartItem.objects.get(id=item_id)
+                            item.quantity += 1
+                            item.user = user
+                            item.save()
+                        else:
+                            cart_item = CartItem.objects.filter(cart=cart)
+                            for item in cart_item:
+                                item.user = user
+                                item.save()
+            except:
+                pass
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                # next=/cart/checkout/
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
-            messages.error(request,'Invalid credentials')
+            messages.error(request, 'Invalid login credentials')
             return redirect('login')
-        
-    return render(request,'accounts/login.html')
+    return render(request, 'accounts/login.html')
+
 
 @login_required(login_url='login')
 def logout(request):
@@ -154,7 +203,7 @@ def resetPassword(request):
         if password == confirm_password:
             uid = request.session.get('uid')
             user = Account.objects.get(pk=uid)
-            user.set_password(password)
+            user.set_password(password) #must be set so it has password
             user.save()
             messages.success(request, 'Password reset successful')
             return redirect('login')
@@ -163,3 +212,5 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
+
+
